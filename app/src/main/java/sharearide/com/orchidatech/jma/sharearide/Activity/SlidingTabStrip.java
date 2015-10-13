b44d006270991918fdb7d52e9215f902738d68a9
Amal <amal_kronz@orchidatech.com>
2015-10-13 12:33:27 +0200

    Amal Kronz 12/10/2015

--- app/src/main/java/sharearide/com/orchidatech/jma/sharearide/Activity/SlidingTabStrip.java
+++ app/src/main/java/sharearide/com/orchidatech/jma/sharearide/Activity/SlidingTabStrip.java
@@ -13,7 +13,7 @@ import android.widget.LinearLayout;
 class SlidingTabStrip extends LinearLayout {
 
     private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 0;
-    private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
+    private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x15;
     private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 0;
     private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFF33B5E5;
 
