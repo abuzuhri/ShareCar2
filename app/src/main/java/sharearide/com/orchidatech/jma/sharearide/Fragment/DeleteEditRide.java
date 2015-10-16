0a2a7ac129910e8199731af2ddbdc9161f9653c8
bahaa_sharqawe <bahaa_sharqawe@orchidatech.com>
2015-10-15 17:16:38 -0700

    BahaaIddin Al Sharqawi 15/10

--- app/src/main/java/sharearide/com/orchidatech/jma/sharearide/Fragment/DeleteEditRide.java
+++ app/src/main/java/sharearide/com/orchidatech/jma/sharearide/Fragment/DeleteEditRide.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
